10e288ac0e479887136e1360beefb396b15b2e4d
Mihail Paladi <40393002+Mibiss@users.noreply.github.com>
2025-08-22 10:55:47 +0200

    added some bootstrap

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res)=>{
     res.render("index.ejs");
