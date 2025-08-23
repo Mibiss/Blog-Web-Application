b02ad5b2ec705815a7888829a95cc6775ae9dfd7
Mihail Paladi <40393002+Mibiss@users.noreply.github.com>
2025-08-23 11:59:16 +0200

    fixed header, footer, storing inputs, added plain about.ejs

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const tweets = [];
+
 
 app.get("/", (req, res)=>{
     res.render("index.ejs");
+});
+
+app.get("/about", (req, res)=>{
+    res.render("about.ejs");
+});
+
+app.post("/Submit", (req, res) => {
+    const name = req.body["name"];
+    const tweet = req.body["content"];
+
+    tweets.push({name, tweet})
 });
 
 app.listen(port, () => {
